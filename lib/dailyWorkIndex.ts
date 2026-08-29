8b1a315dc9f82e2941a10b502ca0c6b2bd1d26c7
challangeguild-ai <challangeguild@gmail.com>
2026-08-29 12:39:26 +0200

    Napi munkavégzés 2.0: utánkövetési javaslat export

--- lib/dailyWorkIndex.ts
+++ lib/dailyWorkIndex.ts
@@ -1,5 +1,6 @@
 export * from "@/lib/dailyWorkPriority";
 export * from "@/lib/dailyWorkFollowUp";
+export * from "@/lib/dailyWorkFollowUpView";
 export * from "@/lib/dailyWorkFlow";
 export * from "@/lib/dailyWorkAlerts";
 export * from "@/lib/dailyWorkPermissions";
